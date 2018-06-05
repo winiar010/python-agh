91815fb6a76006edc1071ccb1f74e84942b19279
Mikolaj Winiarski <zygfrydi@wp.pl>
2018-06-05 11:24:03 +0200

    atak padawanie 3

--- MiTP 22.05/walka&obrona.py
+++
@@ -1,9 +0,0 @@
-
-class Hero(object):
-    def __init__(self, hp=100):
-        self.hp = 100
-    def walcz(self):
-
-
-class Przeciwnik(object, obrona):
-    self.obrona = obrona
---
+++ MiTP 29.05/Hero&Enemy.py
@@ -0,0 +1,63 @@
+#Program na dwie klasy oddziałujące ze sobą
+
+import random
+
+class Hero(object):
+    """Klasa bohatera"""
+    def __init__(self,hp=100):
+        self.hp=hp
+
+    def heal(self):
+        self.hp+=random.randint(1,8)
+        if self.hp>100:
+            self.hp=100
+        print("Lekko się uleczyłeś, bohaterze!")
+
+    def walcz(self,enemy):
+        self.damage = 5 + random.randint(1,20)
+        enemy.obrona(self.damage)
+
+    def obrona (self,damage):
+        print("Ała!")
+        self.hp=self.hp-damage
+        print("Pozostałe hp:",self.hp)
+
+class Enemy(Hero):
+    """Klasa przeciwnika"""
+    def heal(self):
+        self.hp+=random.randint(1,8)
+        if self.hp>50:
+            self.hp=50
+        print("Lekko się uleczyłeś!")
+    def character(self):
+        wybor = ("dobry", "zły")
+        jaki = random.choice(wybor)
+        return jaki
+
+
+
+def walka(hero, enemy):
+    while hero.hp>0 and enemy.hp>0:
+        if enemy.character() == "dobry":
+            pass
+        elif enemy.character() == "zły":
+            print("Uciekamy czym prędzej przyjacielu!")
+            break
+        hero.heal()
+        enemy.heal()
+        hero.walcz(enemy)
+        if enemy.hp<0:
+            break
+        enemy.walcz(hero)
+
+    if hero.hp > 0:
+        print("Zwyciężył bohater!")
+        print("Pozostałe hp bohatera:",hero.hp)
+    else:
+        print("Znowu w życiu Ci nie wyszło")
+        print("Pozostałe hp przeciwnika:",enemy.hp)
+
+#main
+hero=Hero()
+enemy=Enemy(50)
+walka(hero,enemy)
